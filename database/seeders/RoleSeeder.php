76b2c5fee90d802570bbd0bc11cc30880999775b
Demetrio9666 <77036541+Demetrio9666@users.noreply.github.com>
2021-09-23 18:45:16 -0500

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,42 +19,71 @@ class RoleSeeder extends Seeder
         $supervisor = Role::create(['rol'=>'SUPERVISOR']);
         $invitado = Role::create(['rol'=>'INVITADO']);
 
-        Permission::create(['name'=>'fichaAnimal.index','description'=>'Visualizar Ficha de Animales'])->syncRoles([$admin]);
 
-        Permission::create(['name'=>'fichaAnimal.create','description'=>'Crear Ficha de Animales'])->syncRoles([$admin]);
+       
+        Permission::create(['name'=>'fichaAnimal.index',
+                           'description'=>'Visualizar Ficha de Animales'])->syncRoles([$admin]);
+
+        Permission::create(['name'=>'fichaAnimal.create',
+                           'description'=>'Crear Ficha de Animales'])->syncRoles([$admin]);
      
-        Permission::create(['name'=>'fichaAnimal.edit','description'=>'Editar Ficha de Animales'])->syncRoles([$admin]);
+        Permission::create(['name'=>'fichaAnimal.edit',
+                           'description'=>'Editar Ficha de Animales'])->syncRoles([$admin]);
       
-        Permission::create(['name'=>'fichaAnimal.destroy','description'=>'Eliminar Ficha de Animales'])->syncRoles([$admin]);
+        Permission::create(['name'=>'fichaAnimal.destroy',
+                           'description'=>'Eliminar Ficha de Animales'])->syncRoles([$admin]);
         
-        Permission::create(['name'=>'fichaParto.index','description'=>'Visualizar Ficha de Parto'])->syncRoles([$admin]);
+       
+        //////////////////////////////////////////////////////////////////////////////
+        Permission::create(['name'=>'fichaParto.index',
+                           'description'=>'Visualizar Ficha de Parto'])->syncRoles([$admin]);
         
-        Permission::create(['name'=>'fichaParto.create','description'=>'Crear Ficha de Parto'])->syncRoles([$admin]);
+        Permission::create(['name'=>'fichaParto.create',
+                           'description'=>'Crear Ficha de Parto'])->syncRoles([$admin]);
         
-        Permission::create(['name'=>'fichaParto.edit','description'=>'Editar Ficha de Parto'])->syncRoles([$admin]);
+        Permission::create(['name'=>'fichaParto.edit',
+                           'description'=>'Editar Ficha de Parto'])->syncRoles([$admin]);
         
-        Permission::create(['name'=>'fichaParto.destroy','description'=>'Eliminar Ficha de Parto'])->syncRoles([$admin]);
+        Permission::create(['name'=>'fichaParto.destroy',
+                           'description'=>'Eliminar Ficha de Parto'])->syncRoles([$admin]);
+    
+        //////////////////////////////////////////////////////////////////////////////
+
+        Permission::create(['name'=>'fichaTratamiento.index',
+                           'description'=>'Visualizar Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
+    
+        Permission::create(['name'=>'fichaTratamiento.create',
+                           'description'=>'Crear Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
+        
+        Permission::create(['name'=>'fichaTratamiento.edit',
+                           'description'=>'Editar Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
         
 
-        /*$permiso = Permission::create(['name'=>'fichaTratamiento.index','description'=>'Visualizar Ficha de Tratamiento']);
+        Permission::create(['name'=>'fichaTratamiento.destroy',
+                           'description'=>'Eliminar Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
+        
+        //////////////////////////////////////////////////////////////////////////////
 
-        $permiso->syncRole([$admin,$supervisor]);
+        
+        Permission::create(['name'=>'fichaReproduccionM.index',
+                           'description'=>'Visualizar Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
     
-        Permission::create(['name'=>'fichaTratamiento.create','description'=>'Crear Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
+
+        Permission::create(['name'=>'fichaReproduccionM.create',
+                           'description'=>'Crear Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
         
-        Permission::create(['name'=>'fichaTratamiento.edit','description'=>'Editar Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
+
+        Permission::create(['name'=>'fichaReproduccionM.edit',
+                           'description'=>'Editar Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
         
-        Permission::create(['name'=>'fichaTratamiento.destroy','description'=>'Eliminar Ficha de Tratamiento'])->syncRoles([$admin,$supervisor]);
-        
-        Permission::create(['name'=>'fichaReproduccionM.index','description'=>'Visualizar Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
-    
-        Permission::create(['name'=>'fichaReproduccionM.create','description'=>'Crear Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
-        
-        Permission::create(['name'=>'fichaReproduccionM.edit','description'=>'Editar Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
-        
+
         Permission::create(['name'=>'fichaReproduccionM.destroy',
                            'description'=>'Eliminar Ficha Reproducción por Monta Interna'])->syncRoles([$admin,$supervisor,$invitado]);
         
+
+
+        ///////////////////////////////////////////////////////////////////////////////
+
         Permission::create(['name'=>'fichaReproduccionA.index',
                            'description'=>'Visualizar Ficha de Reproducción Artificial'])->syncRoles([$admin,$supervisor,$invitado]);
 
@@ -66,13 +95,16 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name'=>'fichaReproduccionA.destroy',
                            'description'=>'Eliminar Ficha de Reproducción Artificial'])->syncRoles([$admin,$supervisor,]);
-       
+       /////////////////////////////////////////////////////////////////////////////////
+
         Permission::create(['name'=>'fichaReproduccionEx.index',
                            'description'=>'Visualizar Ficha Reproducción Externo'])->syncRoles([$admin,$supervisor,$invitado]);
         
+
         Permission::create(['name'=>'fichaReproduccionEx.create',
                            'description'=>'Crear Ficha Reproducción Externo'])->syncRoles([$admin,$supervisor,$invitado]);
         
+
         Permission::create(['name'=>'fichaReproduccionEx.edit',
                            'description'=>'Editar Ficha Reproducción Externo'])->syncRoles([$admin,$supervisor,$invitado]);
         
@@ -80,6 +112,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'fichaReproduccionEx.destroy',
                            'description'=>'Eliminar Ficha Reproducción Exerno'])->syncRoles([$admin,$supervisor]);
         
+        ////////////////////////////////////////////////////////////////////////////////
       
         Permission::create(['name'=>'controlVacuna.index',
                            'description'=>'Visualizar Control de Vacunación'])->syncRoles([$admin,$supervisor]);
@@ -98,7 +131,7 @@ class RoleSeeder extends Seeder
         
 
 
-      
+        ////////////////////////////////////////////////////////////////////////////////
         Permission::create(['name'=>'controlPeso.index',
                             'description'=>'Visualizar Control de Peso'])->syncRoles([$admin,$supervisor,$invitado]);
 
@@ -111,7 +144,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'controlPeso.destroy',
                             'description'=>'Eliminar Control de Peso'])->syncRoles([$admin,$supervisor,$invitado]);
 
-      
+        ////////////////////////////////////////////////////////////////////////////////
         Permission::create(['name'=>'controlDesparasitacion.index',
                             'description'=>'Visualizar Control de Desparasitación'])->syncRoles([$admin,$supervisor]);
 
@@ -123,7 +156,7 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name'=>'controlDesparasitacion.destroy',
                             'description'=>'Eliminar Control de Desparasitación'])->syncRoles([$admin,$supervisor]);
-     
+        ///////////////////////////////////////////////////////////////////////////////
 
 
         Permission::create(['name'=>'controlPrenes.index',
@@ -138,7 +171,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'controlPrenes.destroy',
                             'description'=>'Eliminar Control Preñez'])->syncRoles([$admin,$supervisor]);
 
-     
+       /////////////////////////////////////////////////////////////////////////////////
 
         Permission::create(['name'=>'confDespa.index',
                             'description'=>'Visualizar Configuración de Desparacitante'])->syncRoles([$admin,$supervisor]);
@@ -151,7 +184,7 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name'=>'confDespa.destroy',
                             'description'=>'Eliminar Configuración de Desparacitante'])->syncRoles([$admin,$supervisor]);
-      
+        /////////////////////////////////////////////////////////////////////////////////
 
         Permission::create(['name'=>'confVacuna.index',
                             'description'=>'Visualizar Configuración de Vacunas'])->syncRoles([$admin,$supervisor]);
@@ -164,7 +197,7 @@ class RoleSeeder extends Seeder
 
         Permission::create(['name'=>'confVacuna.destroy',
                             'description'=>'Eliminar Configuración de Vacunas'])->syncRoles([$admin,$supervisor]);
-       
+        ///////////////////////////////////////////////////////////////////////////////////
 
         Permission::create(['name'=>'confVi.index',
                             'description'=>'Visualizar Configuración de Vitaminas'])->syncRoles([$admin,$supervisor]);
@@ -178,7 +211,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'confVi.destroy',
                             'description'=>'Eliminar Configuración de Vitaminas'])->syncRoles([$admin,$supervisor]);
         
-        
+        //////////////////////////////////////////////////////////////////////////////////
         Permission::create(['name'=>'confAnt.index',
                             'description'=>'Visualizar Configuración de antibióticos'])->syncRoles([$admin,$supervisor]);
 
@@ -191,7 +224,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'confAnt.destroy',
                             'description'=>'Eliminar Configuración de antibióticos'])->syncRoles([$admin,$supervisor]);
 
-        
+        //////////////////////////////////////////////////////////////////////////////////
         
         Permission::create(['name'=>'confMate.index',
                             'description'=>'Visualizar Configuración de Material Genético'])->syncRoles([$admin,$supervisor]);
@@ -206,7 +239,7 @@ class RoleSeeder extends Seeder
                             'description'=>'Eliminar Configuración de Material Genético'])->syncRoles([$admin,$supervisor]);
 
 
-       
+        ///////////////////////////////////////////////////////////////////////////////////
         Permission::create(['name'=>'confUbicacion.index',
                             'description'=>'Visualizar Configuración de Ubicación Interna'])->syncRoles([$admin]);
 
@@ -219,7 +252,7 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'confUbicacion.destroy',
                             'description'=>'Eliminar Configuración de Ubicación Interna'])->syncRoles([$admin]);
 
-        
+        /////////////////////////////////////////////////////////////////////////////////////
         Permission::create(['name'=>'confRaza.index',
                             'description'=>'Visualizar Configuración de Razas'])->syncRoles([$admin]);
 
@@ -232,11 +265,14 @@ class RoleSeeder extends Seeder
         Permission::create(['name'=>'confRaza.destroy',
                             'description'=>'Eliminar Configuración de Razas'])->syncRoles([$admin]);
 
-        
+        ///////////////////////////////////////////////////////////////////////////////////
 
         Permission::create(['name'=>'dashboard.index',
                             'description'=>'Visualizar Dashboards'])->syncRoles([$admin,$supervisor]);
-        
+        //////////////////////////////////////////////////////////////////////////////////
+
+
+        ////////////////////////////////////////////////////////////////////////// 
         Permission::create(['name'=>'rol.index',
                             'description'=>'Visualizar Roles'])->syncRoles([$admin]);
 
@@ -266,7 +302,7 @@ class RoleSeeder extends Seeder
                             'description'=>'Eliminar Usuarios'])->syncRoles([$admin]);
 
         Permission::create(['name'=>'actividad.index',
-                            'description'=>'Visualizar Actividad de Usuario'])->syncRoles([$admin]);*/
+                            'description'=>'Visualizar Actividad de Usuario'])->syncRoles([$admin]);
 
        
         
