310d168bef8cc9170986c5f822a799d440de3cb4
Demetrio9666 <77036541+Demetrio9666@users.noreply.github.com>
2021-09-24 07:28:22 -0500

    1

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['rol'=>'ADMINISTRADOR']);
+        Role::create(['rol'=>'SUPERVISOR']);
+        Role::create(['rol'=>'INVITADO']);
+    }
+}
